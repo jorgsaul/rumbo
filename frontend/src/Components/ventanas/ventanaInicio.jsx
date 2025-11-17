839f2d961db1f8ad76277fdce3d2beeb7398c50a
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-11-17 01:39:15 -0600

    Correccion del error para el deploy

--- frontend/src/Components/ventanas/ventanaInicio.jsx
+++ frontend/src/Components/ventanas/ventanaInicio.jsx
@@ -1,28 +1,30 @@
-import './VentanaInicio.css'
-import CardLogin from '../cards/cardLogin'
-import VentanaRegistro from './ventanaRegistro';
-import VentanaRecuperarContraseña from './ventanaRecuperarContraseña';
+import "./ventanaInicio.css";
+import CardLogin from "../cards/cardLogin";
+import VentanaRegistro from "./ventanaRegistro";
+import VentanaRecuperarContraseña from "./ventanaRecuperarContraseña";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-
-function VentanaInicio({cambiarVentana}){
-  const [vista, setVista] = useState('login');
-  return(
+function VentanaInicio({ cambiarVentana }) {
+  const [vista, setVista] = useState("login");
+  return (
     <div className="contenedor-principal">
-    {vista === 'registro' ? (
-      <div className='overlay-background'>
-        <div className='overlay-content'>
-          <VentanaRegistro cambiarVentana={cambiarVentana} cambiarVista={setVista}/>
+      {vista === "registro" ? (
+        <div className="overlay-background">
+          <div className="overlay-content">
+            <VentanaRegistro
+              cambiarVentana={cambiarVentana}
+              cambiarVista={setVista}
+            />
+          </div>
         </div>
-      </div>
-    ) : vista === 'recuperarCuenta' ? (
-      <div className='overlay-background'>
-        <div className='overlay-content'>
-          <VentanaRecuperarContraseña />
+      ) : vista === "recuperarCuenta" ? (
+        <div className="overlay-background">
+          <div className="overlay-content">
+            <VentanaRecuperarContraseña />
+          </div>
         </div>
-      </div>
-    ): null}
+      ) : null}
       <div className="izquierda"></div>
       <div className="derecha">
         <CardLogin cambiarVista={setVista} cambiarVentana={cambiarVentana} />
