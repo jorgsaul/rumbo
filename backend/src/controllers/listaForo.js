815f920bc87c3dae5d6b242c4224dd61a14fdf1f
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-11-19 03:09:46 -0600

    arreglo

--- backend/src/controllers/listaForo.js
+++ backend/src/controllers/listaForo.js
@@ -4,7 +4,7 @@ async function obtenerPosts(filtro, usuarioIdPerfil, usuarioIdSesion) {
   try {
     const result = await pool.query(`SELECT * FROM spgetusercontent($1, $2, $3)`, [filtro, usuarioIdPerfil, usuarioIdSesion]);
     if(result.rows.length > 0) {
-      return result.rows[0];
+      return result.rows;
     }else{
       return [];
     }
