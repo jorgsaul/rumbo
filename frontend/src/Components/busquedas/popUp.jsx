5664cd2001798d47ec5d1deb3f4857fd044bb102
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-11-25 23:49:07 -0600

    css

--- frontend/src/Components/busquedas/popUp.jsx
+++ frontend/src/Components/busquedas/popUp.jsx
@@ -2,7 +2,12 @@ import "./busquedas.css";
 function PopUpSearch() {
   return (
     <div className="contenedor-popUp">
-      <p>Hola que rollo</p>
+      <h1>Recursos</h1>
+      <p>Todo para recursos</p>
+      <h1>Perfiles</h1>
+      <p>Todo para perfiles</p>
+      <h1>Publicaciones</h1>
+      <p>Todo para publicaciones</p>
     </div>
   );
 }
